cc1786619db91889f43dc663c1f626eca3638806
Matthew Linton <matthew.linton@gmail.com>
2014-06-27 16:08:08 -0400

    Added argument handling to memfill and procloop.

--- src/fillmem.c
+++ src/fillmem.c
@@ -2,17 +2,42 @@
 #include <stdio.h>
 #include <unistd.h>
 
+const char version[] = "1.2.0";
+const char printhelpshort[] = "\
+Usage: fillmem [-h] [-s size]\n\
+Try fillmem -h for more information\n";
+const char printhelplong[] = "\
+Usage: fillmem [-h] [-s size]\n\
+Arguments:\n\
+    -h              Print out this help information.\n\
+    -s <size>       Alter the size (KB) taken up by fillmem.\n\
+                    Default 1KB.";
+
 // Fill memory untill calloc breaks or we kill the process
-
 int main(int argc, char *argv[]) {
-    int scale = 128;
-    int *p;
-    int inc=1024*1024*sizeof(char);
-    
-    if ( argc >= 2 ) {
-        scale = atoi(argv[1]);
-    }
+    unsigned int scale = 128;
+    unsigned int *p;
+    unsigned int inc=1024*1024*sizeof(char);
+    int carg = 0;
 
-    p = (int*) calloc(scale, inc);
+    // get command line arguments
+    while ((carg = getopt(argc, argv, "hs")) != -1) {
+        switch (carg) {
+            case 'h':
+                fprintf(stderr, printhelplong);
+                return 0;
+            case 's':
+                if((unsigned int)optarg >= 0 && (unsigned int)optarg <= 1000000) {
+                    scale = (unsigned int)optarg;
+                }
+                break;
+            case '?':
+                fprintf(stderr, printhelpshort);
+                return 1;
+        }
+    }
+    
+    // allocate memory
+    p = (unsigned int*) calloc(scale, inc);
     pause();    // just sit in memory untill we get killed
 }
--- src/procloop.c
+++ src/procloop.c
@@ -1,17 +1,49 @@
 #include <stdlib.h>
-#include <math.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <math.h>
+
+const char version[] = "1.2.0";
+const char printhelpshort[] = "\
+Usage: procloop [-h] [-s microseconds]\n\
+Try procloop -h for more information\n";
+const char printhelplong[] = "\
+Usage: procloop [-h] [-s microseconds]\n\
+Arguments:\n\
+    -h              Print out this help information.\n\
+    -s <usec>       Sleep for the number of microseconds specified.\n\
+                    This can be between 1 - 1000000. default 1 second.\n";
 
 // run an processor intensive loop
-
 int main(int argc, char *argv[]) {
+    unsigned int sleep = 1000000;      // microseconds to sleep
+    int carg = 0;
     double res = 5;
     double i = 5;
+
+    // get command line arguments
+    while ((carg = getopt(argc, argv, "hs")) != -1) {
+        switch (carg) {
+            case 'h':
+                fprintf(stderr, printhelplong);
+                return 0;
+            case 's':
+                if((unsigned int)optarg >= 0 && (unsigned int)optarg <= 1000000) {
+                    sleep = (unsigned int)optarg;
+                }
+                break;
+            case '?':
+                fprintf(stderr, printhelpshort);
+                return 1;
+        }
+    }
+
+    // Tie up the processor 
     while(1) {
         res = fmod(res, i);
         i = pow(res, i);
         res = exp(i);
         i = fmod(res, i);
-        usleep(1);     // sleep for 1 micro second to let other processes do stuff
+        usleep(sleep);
     }
 }
